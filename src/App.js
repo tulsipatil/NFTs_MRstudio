1725487dc214a6f9d1004e6c4eaf82acbe5d5893
tulsi <tulsi.patil2021@vitbhopal.ac.in>
2022-12-13 21:23:19 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Content from './components/Content'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <div className='container mt-3 mb-3'>
+    <Navbar/>
+    <Content/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div className="container">
+        <div className="main text-center">
+            <br />
+            <br />
+            <h3>
+                Collect and sell super rate NFTs
+            </h3>
+            <p className='text-secondary'>
+                The world's first and largest marketplace for crypto collectibles and non-fungible tokens(NFTs).
+            </p>
+            <br />
+            <br />
+            <button className="btn btn-outline-secondary">Explore Marketplace</button>
+            <br />
+            <br />
+            <div className="row text-center">
+                <div className="col shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="btn btn-outline-secondary">44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+                <div className="col shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="btn btn-outline-secondary">44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+                <div className="col shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="btn btn-outline-secondary">44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
